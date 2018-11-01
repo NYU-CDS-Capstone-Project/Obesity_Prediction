6a5dad821bc74b7dcacc1587986ca73283038376
Daoyang Shan <szyyn95@gmail.com>
2018-11-01 14:02:23 -0400

    integrated code v1.0

---
+++ src/config.py
@@ -0,0 +1,54 @@
+icd9List = '../auxdata/icd9listccs.txt'
+icd10List = '../auxdata/icd10listccs.txt'
+genderList = '../auxdata/genderlist.txt'
+ethnicityList = '../auxdata/ethnicityList.txt'
+raceList = '../auxdata/raceList.txt'
+zipList = '../auxdata/zipList.txt'
+vitalsList = '../auxdata/vitals_subset.txt'
+vitalsGainsList = '../auxdata/vitals_subset_gains.txt'
+vitalsZScoreList = '../auxdata/vitals_wt_bmi_zscore.txt'
+labslist = '../auxdata/labs.txt'
+medslist = '../auxdata/meds.txt'
+
+CDC_BMI_Ref = '../auxdata/CDC_BMIs.txt'
+CDC_BMI_95 = '../auxdata/CDC_BMI_95.txt'
+wght4leng_girl = '../auxdata/WHO_wfl_girls_p_exp.txt'
+wght4leng_boy = '../auxdata/WHO_wfl_boys_p_exp.txt'
+bmi_girl = '../auxdata/CDC_bmi_age_girls.csv'
+bmi_boy = '../auxdata/CDC_bmi_age_boys.csv'
+
+timeList = '../auxdata/time_list.txt'  
+
+#maternal info
+input_csv_mothers = ['']
+input_csv_mothers_delim = ','
+
+input_csv_mothers_MRN = 'BM_MRN'
+input_csv_newborn_MRN = 'NB_MRN'
+input_csv_mothers_agedeliv = 'BM_Age_at_Delivery' #int
+input_csv_mothers_marriage = 'BM_Marital_Status'
+input_csv_mothers_race = 'BM_Race'
+input_csv_mothers_ethn = 'BM_Ethnicity'
+input_csv_mothers_national = 'BM_Nationality'
+input_csv_mothers_birthplace = 'BM_BirthPlace'
+input_csv_mothers_lang = 'BM_Language'
+input_csv_mothers_insur1 = 'BM_Prim_Ins'
+input_csv_mothers_insur2 = 'BM_Second_Ins'
+input_csv_mothers_diags = 'MAT_ACCT_DAIG_LIST'# (icd9/10 list separated by ';')
+input_csv_mothers_NB_diags = 'NEW ACCT DIAG List'# (icd9/10 list separated by ';')
+input_csv_mothers_deliv_diags = 'Delv DIAG'# (icd9/10 list separated by ';')
+
+
+BM_Marital_StatusList = '../auxdata/BM_Marital_Status.txt'
+BM_RaceList = '../auxdata/BM_Race.txt'
+BM_EthnicityList = '../auxdata/BM_Ethnicity.txt'
+BM_NationalityList = '../auxdata/BM_Nationality.txt'
+BM_BirthPlace = '../auxdata/BM_BirthPlace.txt'
+BM_Language = '../auxdata/BM_Language.txt'
+BM_Prim_Ins = '../auxdata/BM_Prim_Ins.txt'
+BM_Second_Ins = '../auxdata/BM_Second_Ins.txt'
+
+# historical maternal info
+BM_Labs = '../auxdata/BM_Labs.txt'
+# BM_Meds = '../auxdata/BM_Meds.txt'
+BM_Procedures = '../auxdata/BM_Procedures.txt'
